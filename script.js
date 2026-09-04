2480119fdb980e24d72202ebf0b4fcf4d33718e0
yabeshchy1000 <yabeshchaudhary888@gmail.com>
2026-09-04 18:13:57 +0545

    add error types, error handling, spread vs rest

--- script.js
+++ script.js
@@ -182,12 +182,155 @@
 // let result = first.concat(" ", second);
 // console.log(result);
 
-// using template literals
-let name = "John";
-let age = 20;
-console.log(`${name} is ${age} years old`);
+// // using template literals
+// let name = "John";
+// let age = 20;
+// console.log(`${name} is ${age} years old`);
 
 
-console.log("Hello World");
+// console.log("Hello World");
 
 
+
+// // Error in JS
+
+
+// // SyntaxError
+
+// let name = "Yabesh";
+// console.log(name;
+
+// try {
+//   eval("let x = ;");
+// } catch (error) {
+//   console.log(error.name);
+// }
+
+// // ReferenceError
+
+// console.log(username);
+
+// try {
+//   console.log(username);
+// } catch (error) {
+//   console.log(error.name);
+// }
+
+// // TypeError
+// let name = "John";
+// name();
+
+// let user = null;
+// console.log(user.name);
+
+
+// // // RangeError
+// let num = 10.123;
+// console.log(num.toFixed(200));
+
+// // URIError
+// decodeURIComponent("%");
+
+// // EvalError
+// throw new EvalError("problem with eval");
+
+
+
+// // // Error Handling
+
+// 1. try....catch
+
+// try {
+//   let result = undefinedVariable;
+//   console.log(result);
+// } catch (error) {
+//   console.log("Something went wrong");
+//   console.log(error.name);
+// }
+
+// try {
+//   console.log(undefinedVariable);
+// } catch (error) {
+//   console.log(error.name);
+//   console.log(error.message);
+// }
+
+
+// // 2. finally
+// try {
+//   console.log("Trying...");
+// } catch (error) {
+//   console.log("Error Occurred");
+// } finally {
+//   console.log("Finished");
+// }
+
+
+// // 3. throw
+// let age = -5;
+// try {
+//   if (age < 0) {
+//     throw new Error("Age can't be -ve");
+//   }
+// } catch (error) {
+//   console.log(error.message);
+// }
+
+
+// // shallow copy
+// // creates new outer object, but nested objects are still shared
+// const user = {
+//   name: "Yabesh",
+//   address: {
+//     city: "Dhangadhi"
+//   }
+// };
+
+// const copy = Object.assign({}, user);
+
+// copy.name = "Prabhat";
+// copy.address.city = "Attariya";
+
+// console.log(user);
+
+// // deep copy
+// // creates a complete independent copy, including nested objects and arrays
+// const user = {
+//   name: "Yabesh",
+//   address: {
+//     city: "Dhangadhi"
+//   }
+// };
+// const copy = structuredClone(user);
+// copy.address.city = "Attariya";
+// console.log(user.address.city);
+// // the original is unaffected
+
+
+// // // spread operator
+
+// const fruits = ["Apple", "Banana", "Mango"];
+// const newFruits = [...fruits, "Orange"];
+// console.log(newFruits);
+
+// const user = {
+//   name: "Yabesh",
+//   age: 22
+// };
+// const newUser = {
+//   ...user,
+//   city: "Dhangadhi"
+// };
+// console.log(newUser);
+
+// // rest operator 
+
+// function add (...numbers) {
+//   console.log(numbers);
+// }
+// add(10,20,30,40);
+
+// function add(...numbers) {
+//   return numbers.reduce((sum,num) => sum+num, 0);
+// }
+// console.log(add(10,20,30));
