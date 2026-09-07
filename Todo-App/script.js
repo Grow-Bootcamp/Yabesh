b5da97397f00467ee0c7bf84b5aa34a46f1d305e
yabeshchy1000 <yabeshchaudhary888@gmail.com>
2026-09-07 18:23:26 +0545

    Interface for Todo App

---
+++ Todo-App/script.js
@@ -0,0 +1,16 @@
+// let tasks = [];
+// // tasks.push("Learn HTML");
+// // tasks.push("Learn CSS");
+// // tasks.push("Learn JS");
+
+
+// const form = document.querySelector("form");
+// const input = document.querySelector("#task-input");
+// const taskList = document.querySelector("#task-list");
+
+// form.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   const taskText = input.value.trim();
+//   console.log(taskText);
+// });
+
